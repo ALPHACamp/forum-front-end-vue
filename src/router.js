0b862c20f64d62f61ebf3396901b3547ad0c8b65
pjchender <pjchender@gmail.com>
2019-05-19 09:12:43 +0800

    Setting router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Layout from './views/Layout.vue'
 
 Vue.use(Router)
 
@@ -10,16 +11,21 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: Layout,
+      name: 'layout',
+      children: [
+        {
+          path: '/home',
+          name: 'home',
+          component: Home
+        },
+        {
+          path: '/about',
+          name: 'about',
+          component: () =>
+            import(/* webpackChunkName: "about" */ './views/About.vue')
+        }
+      ]
     }
   ]
 })
---
+++ src/views/Layout.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+      <a class="navbar-brand" href="/">餐廳評論網</a>
+      <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto"></ul>
+        <div>
+          <!--
+        {{#if user.isAdmin}}
+          <a href="/admin" style="color: white;margin-right: 10px;">Admin 後台</a>
+        {{/if}}
+        {{#if user}}
+          <span style="color: white;margin-right: 10px;">Hi, {{user.email}}</span>
+          <a href="/users/{{user.id}}" style="color: white;margin-right: 10px;">Profile</a>
+          <a href="/logout"><button class="btn btn-outline-success my-2 my-sm-0">LogOut</button></a>
+        {{/if}}
+          -->
+        </div>
+      </div>
+    </nav>
+    <br>
+
+    <main role="main">
+      <div class="album py-5 bg-light">
+        <div class="container">
+          <!--
+          {{#if success_messages}}
+            <div class="alert alert-success" role="alert" style="margin: 30px 0 ;">
+              {{success_messages}}
+            </div>
+          {{/if}}
+          {{#if error_messages}}
+            <div class="alert alert-danger" role="alert" style="margin: 30px 0 ;">
+              {{error_messages}}
+            </div>
+          {{/if}}
+          -->
+          <router-view />
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>

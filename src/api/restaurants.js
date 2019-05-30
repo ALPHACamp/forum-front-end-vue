ec007a9a48326419ff661e3f0ccf08ec553e99f3
pjchender <pjchender@gmail.com>
2019-05-30 21:23:45 +0800

    Add RestaurantsDashbaord Page

--- src/api/restaurants.js
+++ src/api/restaurants.js
@@ -1,12 +1,12 @@
 import { apiHelper } from '@/utils/helpers'
 
 export default {
+  getRestaurant({ restaurantId }) {
+    return apiHelper.get(`/restaurants/${restaurantId}`)
+  },
   getRestaurants({ page, categoryId }) {
     const searchParams = new URLSearchParams({ page, categoryId })
     return apiHelper.get(`/restaurants?${searchParams.toString()}`)
-  },
-  getRestaurant({ restaurantId }) {
-    return apiHelper.get(`/restaurants/${restaurantId}`)
   },
   getFeeds() {
     return apiHelper.get('/restaurants/feeds')
--- src/views/RestaurantsDashboard.vue
+++ src/views/RestaurantsDashboard.vue
@@ -1,11 +1,73 @@
 <template>
   <div>
-    <h1>This is RestaurantsDashboard page</h1>
+    <div>
+      <h1>{{ restaurant.name }}</h1>
+      <p>[{{ restaurant.categoryName }}]</p>
+    </div>
+
+    <hr>
+
+    <ul>
+      <li>有 {{ restaurant.commentsLength }} 筆評論</li>
+      <li>有 {{ restaurant.favoritedUsersLength }} 人收藏這家餐廳</li>
+    </ul>
+
+    <a href="#" @click="$router.back()">回上一頁</a>
   </div>
 </template>
 
 <script>
+import { Toast } from '@/utils/helpers'
+import restaurantsAPI from '@/api/restaurants'
+
 export default {
-  name: 'RestaurantsDashboard'
+  name: 'RestaurantsDashboard',
+  data() {
+    return {
+      restaurant: {
+        id: -1,
+        name: '',
+        categoryName: '',
+        commentsLength: 0,
+        favoritedUsersLength: 0,
+        likedUsersLength: 0
+      }
+    }
+  },
+  mounted() {
+    const { id: restaurantId } = this.$route.params
+    this.fetchRestaurant(restaurantId)
+  },
+  methods: {
+    async fetchRestaurant(restaurantId) {
+      try {
+        const {
+          data: { restaurant },
+          statusText
+        } = await restaurantsAPI.getRestaurant({
+          restaurantId
+        })
+
+        if (statusText !== 'OK') {
+          throw new Error(statusText)
+        }
+
+        this.restaurant = {
+          ...this.restaurant,
+          id: restaurant.id,
+          name: restaurant.name,
+          categoryName: restaurant.Category.name,
+          commentsLength: restaurant.Comments.length,
+          favoritedUsersLength: restaurant.FavoritedUsers.length,
+          likedUsersLength: restaurant.LikedUsers.length
+        }
+      } catch (error) {
+        Toast.fire({
+          type: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
+      }
+    }
+  }
 }
 </script>

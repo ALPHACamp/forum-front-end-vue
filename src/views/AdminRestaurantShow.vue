4411883b2ac0f25e9255e5017dd14bec8838b465
pjchender <pjchender@gmail.com>
2019-06-01 19:27:10 +0800

    Finish AdminRestaurantShow Page

--- src/views/AdminRestaurantShow.vue
+++ src/views/AdminRestaurantShow.vue
@@ -1,11 +1,107 @@
 <template>
   <div>
-    <h1>This is AdminRestaurantShow page</h1>
+    <div class="row">
+      <div class="col-md-12">
+        <h1>{{restaurant.name}}</h1>
+        <p>[{{restaurant.categoryName}}]</p>
+      </div>
+      <div class="col-md-4">
+        <img
+          class="img-responsive center-block"
+          :src="restaurant.image | emptyImage"
+          style="width: 250px;margin-bottom: 25px;"
+        >
+        <div class="well">
+          <ul class="list-unstyled">
+            <li>
+              <strong>Opening Hour:</strong>
+              {{restaurant.openingHours}}
+            </li>
+            <li>
+              <strong>Tel:</strong>
+              {{restaurant.tel}}
+            </li>
+            <li>
+              <strong>Address:</strong>
+              {{restaurant.address}}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-8">
+        <p>{{restaurant.description}}</p>
+      </div>
+    </div>
+    <hr>
+
+    <a href="#" @click="$router.back()">回上一頁</a>
   </div>
 </template>
 
 <script>
+import adminAPI from '@/api/admin'
+import { emptyImageFilter } from '@/utils/mixin'
+import { Toast } from '@/utils/helpers'
+
 export default {
-  name: 'AdminRestaurantShow'
+  name: 'AdminRestaurantShow',
+  mixins: [emptyImageFilter],
+  data() {
+    return {
+      restaurant: {
+        id: -1,
+        name: '',
+        categoryName: '',
+        image: '',
+        openingHours: '',
+        tel: '',
+        address: '',
+        description: ''
+      }
+    }
+  },
+  mounted() {
+    const { id: restaurantId } = this.$route.params
+    this.fetchRestaurant(restaurantId)
+  },
+  beforeRouteUpdate(to, from, next) {
+    // 路由改變時重新抓取資料
+    const { id: restaurantId } = to.params
+    this.fetchRestaurant(restaurantId)
+    next()
+  },
+  methods: {
+    async fetchRestaurant(restaurantId) {
+      try {
+        const {
+          data: { restaurant },
+          statusText
+        } = await adminAPI.restaurants.getDetail({
+          restaurantId
+        })
+
+        if (statusText !== 'OK') {
+          throw new Error(statusText)
+        }
+
+        this.restaurant = {
+          ...this.restaurant,
+          id: restaurant.id,
+          name: restaurant.name,
+          categoryName: restaurant.Category.name,
+          image: restaurant.image,
+          openingHours: restaurant.opening_hours,
+          tel: restaurant.tel,
+          address: restaurant.address,
+          description: restaurant.description
+        }
+      } catch (error) {
+        Toast.fire({
+          type: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
+      }
+    }
+  }
 }
 </script>

2d231968d23bc636e3916bde3d3701e171de885b
Rafeni <rafeni@riease.com>
2019-06-08 09:32:41 +0800

    Revert "調整 Restaurants.vue 與 RestaurantsShow.vue UI"
    This reverts commit 58fa0d19207bb2c7e7c0d56a960013b912e9df05.

--- src/components/RestaurantComments.vue
+++ src/components/RestaurantComments.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2 class="my-4">所有評論</h2>
+    <h3 class="my-4">所有評論：</h3>
 
     <div v-for="comment in restaurantComments" :key="comment.id">
       <blockquote class="blockquote mb-0">
@@ -10,11 +10,11 @@
           type="button"
           class="btn btn-danger float-right"
         >Delete</button>
-        <h3>
+        <h4>
           <router-link
             :to="{ name: 'user-show', params: { id: comment.User.id } }"
           >{{ comment.User.name }}</router-link>
-        </h3>
+        </h4>
         <p>{{comment.text}}</p>
         <footer class="blockquote-footer">{{comment.createdAt | fromNow }}</footer>
       </blockquote>
@@ -24,13 +24,13 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
-import { Toast } from "@/utils/helpers";
-import { fromNowFilter } from "@/utils/mixin";
-import commentsAPI from "@/api/comments";
+import { mapState } from 'vuex'
+import { Toast } from '@/utils/helpers'
+import { fromNowFilter } from '@/utils/mixin'
+import commentsAPI from '@/api/comments'
 
 export default {
-  name: "RestaurantComments",
+  name: 'RestaurantComments',
   mixins: [fromNowFilter],
   props: {
     restaurantComments: {
@@ -39,54 +39,33 @@ export default {
     }
   },
   computed: {
-    ...mapState(["currentUser"])
+    ...mapState(['currentUser'])
   },
   methods: {
     async removeComment(commentId) {
       try {
         const { data, statusText } = await commentsAPI.remove({
           commentId
-        });
-        if (statusText !== "OK" || data.status !== "success") {
-          throw new Error(statusText);
+        })
+        if (statusText !== 'OK' || data.status !== 'success') {
+          throw new Error(statusText)
         }
 
-        this.$emit("after-remove-comment", {
+        this.$emit('after-remove-comment', {
           commentId
-        });
+        })
 
         Toast.fire({
-          type: "success",
-          title: "移除評論成功"
-        });
+          type: 'success',
+          title: '移除評論成功'
+        })
       } catch (error) {
         Toast.fire({
-          type: "error",
-          title: "無法移除評論，請稍後再試"
-        });
+          type: 'error',
+          title: '無法移除評論，請稍後再試'
+        })
       }
     }
   }
-};
+}
 </script>
-<style lang="css" scoped>
-h2.my-4 {
-  margin-bottom: 1.1rem !important;
-  font-size: 18px;
-}
-h3 {
-  margin-bottom: 3px;
-  line-height: 1.3;
-}
-.swal2-icon.swal2-success [class^="swal2-success-line"] {
-  background-color: #bd2333 !important;
-}
-.swal2-popup.swal2-toast .swal2-success,
-.swal2-icon.swal2-success .swal2-success-ring {
-  border-color: rgba(189, 35, 51, 0.3) !important;
-}
-button.btn,
-.text-right button.btn {
-  margin: 0 !important;
-}
-</style>

12275c43869f7251e478c640855eb6eb8776440c
Rafeni <rafeni@riease.com>
2019-06-11 20:35:41 +0800

    調整 UI 細節
    1. 確保沒有影響到 JS 的 coding style (感謝 PJ 協助)
    2. 移除 CSS 瀏覽器前綴
    3. 調整 .will 為 .contact-info-wrap

--- src/views/RestaurantShow.vue
+++ src/views/RestaurantShow.vue
@@ -11,7 +11,7 @@
           :src="restaurant.image | emptyImage"
           style="width: 250px;margin-bottom: 25px;"
         >
-        <div class="well">
+        <div class="contact-info-wrap">
           <ul class="list-unstyled">
             <li>
               <strong>Opening Hour:</strong>
@@ -85,16 +85,16 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
-import RestaurantComments from "@/components/RestaurantComments";
-import CreateComment from "@/components/CreateComment";
-import restaurantsAPI from "@/api/restaurants";
-import usersAPI from "@/api/users";
-import { Toast } from "@/utils/helpers";
-import { emptyImageFilter } from "@/utils/mixin";
+import { mapState } from 'vuex'
+import RestaurantComments from '@/components/RestaurantComments'
+import CreateComment from '@/components/CreateComment'
+import restaurantsAPI from '@/api/restaurants'
+import usersAPI from '@/api/users'
+import { Toast } from '@/utils/helpers'
+import { emptyImageFilter } from '@/utils/mixin'
 
 export default {
-  name: "RestaurantShow",
+  name: 'RestaurantShow',
   components: {
     RestaurantComments,
     CreateComment
@@ -104,35 +104,35 @@ export default {
     return {
       restaurant: {
         id: -1,
-        name: "",
-        categoryName: "",
-        image: "",
-        openingHours: "",
-        tel: "",
-        address: "",
-        description: "",
+        name: '',
+        categoryName: '',
+        image: '',
+        openingHours: '',
+        tel: '',
+        address: '',
+        description: '',
         isFavorited: false,
         isLiked: false
       },
       restaurantComments: []
-    };
+    }
   },
   computed: {
-    ...mapState(["currentUser"])
+    ...mapState(['currentUser'])
   },
   mounted() {
-    const { id: restaurantId } = this.$route.params;
-    this.fetchRestaurant(restaurantId);
+    const { id: restaurantId } = this.$route.params
+    this.fetchRestaurant(restaurantId)
   },
   methods: {
     async fetchRestaurant(restaurantId) {
       try {
         const { data, statusText } = await restaurantsAPI.getRestaurant({
           restaurantId
-        });
+        })
 
-        if (statusText !== "OK") {
-          throw new Error(statusText);
+        if (statusText !== 'OK') {
+          throw new Error(statusText)
         }
 
         this.restaurant = {
@@ -147,18 +147,18 @@ export default {
           description: data.restaurant.description,
           isFavorited: data.isFavorited,
           isLiked: data.isLiked
-        };
+        }
 
-        this.restaurantComments = data.restaurant.Comments;
+        this.restaurantComments = data.restaurant.Comments
       } catch (error) {
         Toast.fire({
-          type: "error",
-          title: "無法取得餐廳資料，請稍後再試"
-        });
+          type: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
       }
     },
     createComment(payload) {
-      const { restaurantId, text, commentId } = payload;
+      const { restaurantId, text, commentId } = payload
 
       this.restaurantComments.push({
         id: commentId,
@@ -169,117 +169,116 @@ export default {
         },
         text,
         createdAt: new Date()
-      });
+      })
     },
     removeComment(payload) {
-      const { commentId } = payload;
+      const { commentId } = payload
 
       this.restaurantComments = this.restaurantComments.filter(
         comment => comment.id !== commentId
-      );
+      )
     },
     async addFavorite(restaurantId) {
       try {
         const { data, statusText } = await usersAPI.addFavorite({
           restaurantId
-        });
+        })
 
-        if (statusText !== "OK" || data.status !== "success") {
-          throw new Error(statusText);
+        if (statusText !== 'OK' || data.status !== 'success') {
+          throw new Error(statusText)
         }
 
         this.restaurant = {
           ...this.restaurant,
           isFavorited: true
-        };
+        }
       } catch (error) {
         Toast.fire({
-          type: "error",
-          title: "無法將餐廳加入最愛，請稍後再試"
-        });
+          type: 'error',
+          title: '無法將餐廳加入最愛，請稍後再試'
+        })
       }
     },
     async removeFavorite(restaurantId) {
       try {
         const { data, statusText } = await usersAPI.removeFavorite({
           restaurantId
-        });
+        })
 
-        if (statusText !== "OK" || data.status !== "success") {
-          throw new Error(statusText);
+        if (statusText !== 'OK' || data.status !== 'success') {
+          throw new Error(statusText)
         }
 
         this.restaurant = {
           ...this.restaurant,
           isFavorited: false
-        };
+        }
       } catch (error) {
         Toast.fire({
-          type: "error",
-          title: "無法將餐廳從最愛移除，請稍後再試"
-        });
+          type: 'error',
+          title: '無法將餐廳從最愛移除，請稍後再試'
+        })
       }
     },
     async addLike(restaurantId) {
       try {
-        const { data, statusText } = await usersAPI.addLike({ restaurantId });
+        const { data, statusText } = await usersAPI.addLike({ restaurantId })
 
-        if (statusText !== "OK" || data.status !== "success") {
-          throw new Error(statusText);
+        if (statusText !== 'OK' || data.status !== 'success') {
+          throw new Error(statusText)
         }
 
         this.restaurant = {
           ...this.restaurant,
           isLiked: true
-        };
+        }
       } catch (error) {
         Toast.fire({
-          type: "error",
-          title: "無法按讚，請稍後再試"
-        });
+          type: 'error',
+          title: '無法按讚，請稍後再試'
+        })
       }
     },
     async removeLike(restaurantId) {
       try {
         const { data, statusText } = await usersAPI.removeLike({
           restaurantId
-        });
+        })
 
-        if (statusText !== "OK" || data.status !== "success") {
-          throw new Error(statusText);
+        if (statusText !== 'OK' || data.status !== 'success') {
+          throw new Error(statusText)
         }
 
         this.restaurant = {
           ...this.restaurant,
           isLiked: false
-        };
+        }
       } catch (error) {
         Toast.fire({
-          type: "error",
-          title: "無法取消按讚，請稍後再試"
-        });
+          type: 'error',
+          title: '無法取消按讚，請稍後再試'
+        })
       }
     }
   }
-};
+}
 </script>
 <style lang="css" scoped>
 h1 {
   margin-bottom: 3px;
 }
+.badge.badge-secondary {
+  font-size: 12.5px;
+}
 .col-lg-8 p,
-.well li,
-.well strong {
-  font-family: "noto_serif", serif;
+.contact-info-wrap li,
+.contact-info-wrap strong {
+  font-family: 'noto_serif', serif;
   font-size: 17px;
 }
 .pagination {
-  -webkit-box-pack: start;
-  -ms-flex-pack: start;
+  display: flex;
   justify-content: start;
   margin-top: 6px;
-}
-.badge.badge-secondary {
-  font-size: 12.5px;
 }
 </style>

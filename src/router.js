a7f57823422b97f00d43ca962dc66e0ea4718409
pjchender <pjchender@gmail.com>
2019-06-16 15:06:43 +0800

    Remove redundant files

--- src/router.js
+++ src/router.js
@@ -1,23 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import NotFound from './views/NotFound.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () =>
-        import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
     {
       path: '*',
       name: 'not-found',
--- src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <button type="button" class="btn btn-info">Info</button>
-  </div>
-</template>

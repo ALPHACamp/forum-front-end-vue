72aebe3a05db4ad70c22b6bc70fded3d368bf573
pjchender <pjchender@gmail.com>
2019-06-16 12:18:22 +0800

    Install Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,13 @@
   </div>
 </template>
 
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css'
+export default {
+  name: 'App'
+}
+</script>
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button type="button" class="btn btn-info">Info</button>
   </div>
 </template>

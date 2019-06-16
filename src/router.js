763bb71486911517a96d57332430e2be88c737fd
pjchender <pjchender@gmail.com>
2019-06-16 19:19:59 +0800

    Add LoginPage

--- src/router.js
+++ src/router.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import NotFound from './views/NotFound.vue'
+import SignIn from './views/SignIn.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/signin',
+      name: 'sign-in',
+      component: SignIn
+    },
     {
       path: '*',
       name: 'not-found',
---
+++ src/views/Temp.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container py-5">
+    <!-- ... -->
+    <div class="text-center mb-3">
+      <p>
+        <router-link to="/signup">
+          SignUp
+        </router-link>
+      </p>
+    </div>
+    <!-- ... -->
+  </div>
+</template>
+
+<script>
+export default {
+
+  methods: {
+    handleSubmit (e) {
+      const form = new FormData(e.target)
+      this.sendAJAXRequest(form)
+    },
+    sendAJAXRequest (form) {
+      const url = 'http://localhost:3000'
+      fetch(url, {
+        method: 'POST',
+        body: form
+      })
+    }
+  }
+}
+</script>

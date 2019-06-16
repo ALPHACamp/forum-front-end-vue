b17503b70326c1278d4d967bc471d3c08b6592f8
pjchender <pjchender@gmail.com>
2019-06-16 19:48:46 +0800

    Add SignUp Page

--- src/router.js
+++ src/router.js
@@ -13,6 +13,11 @@ export default new Router({
       component: SignIn
     },
     {
+      path: '/signup',
+      name: 'sign-up',
+      component: () => import('./views/SignUp.vue')
+    },
+    {
       path: '*',
       name: 'not-found',
       component: NotFound

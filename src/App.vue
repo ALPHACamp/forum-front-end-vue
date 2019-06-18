88cf9ca16b2cdc80a1c955d3087e0dc8af620605
pjchender <pjchender@gmail.com>
2019-06-18 21:23:55 +0800

    Add Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
+// ./src/App.vue
 <template>
   <div id="restaurant-forum">
-    <router-view />
+    <navbar />
+
+    <main
+      role="main"
+      class="mt-5 bg-white"
+    >
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,92 @@
+<template>
+  <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+    <a
+      class="navbar-brand"
+      href="/"
+    >餐廳評論網</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+
+    <div
+      id="navbarSupportedContent"
+      class="navbar-collapse collapse"
+    >
+      <div class="ml-auto d-flex align-items-center">
+        <!-- is user is admin -->
+        <router-link
+          v-if="currentUser.isAdmin"
+          to="#"
+          class="text-white mr-3"
+        >
+          管理員後台
+        </router-link>
+
+        <!-- is user is login -->
+        <template v-if="isAuthenticated">
+          <router-link
+            to="#"
+            class="text-white mr-3"
+          >
+            {{ currentUser.name || '使用者' }} 您好
+          </router-link>
+          <button
+            type="button"
+            class="btn btn-sm btn-outline-success my-2 my-sm-0"
+          >
+            登出
+          </button>
+        </template>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+const dummyUser = {
+  currentUser: {
+    id: 1,
+    name: '管理者',
+    email: 'root@example.com',
+    image: 'https://i.pravatar.cc/300',
+    isAdmin: true
+  },
+  isAuthenticated: true
+}
+
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+      currentUser: {
+        id: -1,
+        name: '',
+        email: '',
+        image: '',
+        isAdmin: false
+      },
+      isAuthenticated: false
+    }
+  },
+  created () {
+    this.fetchUser()
+  },
+  methods: {
+    fetchUser () {
+      this.currentUser = {
+        ...this.currentUser,
+        ...dummyUser.currentUser
+      }
+      this.isAuthenticated = dummyUser.isAuthenticated
+    }
+  }
+}
+</script>

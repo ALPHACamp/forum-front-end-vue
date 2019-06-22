cff48dc3d1c31e04ae527359009e815b91e4119e
pjchender <pjchender@gmail.com>
2019-06-22 16:50:04 +0800

    Finish RestaurantCard component

---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="col-md-6 col-lg-4">
+    <div class="card mb-4">
+      <img
+        class="card-img-top"
+        :src="restaurant.image"
+        alt="Card image cap"
+        width="286px"
+        height="180px"
+      >
+      <div class="card-body">
+        <p class="card-text title-wrap">
+          <a href="#">
+            {{ restaurant.name }}
+          </a>
+        </p>
+        <span class="badge badge-secondary">{{ restaurant.Category.name }}</span>
+        <p class="card-text text-truncate">
+          {{ restaurant.description }}
+        </p>
+      </div>
+      <div class="card-footer">
+        <button
+          v-if="restaurant.isFavorited"
+          type="button"
+          class="btn btn-danger btn-border favorite mr-2"
+          @click.stop.prevent="removeFavorite(restaurant.id)"
+        >
+          移除最愛
+        </button>
+        <button
+          v-else
+          type="button"
+          class="btn btn-primary btn-border favorite mr-2"
+          @click.stop.prevent="addFavorite(restaurant.id)"
+        >
+          加到最愛
+        </button>
+        <button
+          v-if="restaurant.isLiked"
+          type="button"
+          class="btn btn-danger like mr-2"
+          @click.stop.prevent="removeLike(restaurant.id)"
+        >
+          Unlike
+        </button>
+        <button
+          v-else
+          type="button"
+          class="btn btn-primary like mr-2"
+          @click.stop.prevent="addLike(restaurant.id)"
+        >
+          Like
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RestaurantCard',
+  props: {
+    initialRestaurant: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      restaurant: this.initialRestaurant
+    }
+  },
+  methods: {
+    addFavorite () {
+      this.restaurant = {
+        ...this.restaurant, // 包留餐廳內原本的資料
+        isFavorited: true
+      }
+    },
+    removeFavorite () {
+      this.restaurant = {
+        ...this.restaurant, // 包留餐廳內原本的資料
+        isFavorited: false
+      }
+    },
+    addLike () {
+      this.restaurant = {
+        ...this.restaurant,
+        isLiked: true
+      }
+    },
+    removeLike () {
+      this.restaurant = {
+        ...this.restaurant,
+        isLiked: false
+      }
+    }
+  }
+}
+</script>

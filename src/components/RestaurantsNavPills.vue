7121e065c6ab2b72114f532cbdd8597b28c2abc5
pjchender <pjchender@gmail.com>
2019-06-22 17:33:34 +0800

    Add RestaurantsNavPills component

---
+++ src/components/RestaurantsNavPills.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="nav nav-pills mb-4">
+    <li class="nav-item">
+      <router-link
+        class="nav-link"
+        :to="{ name: 'restaurants' }"
+      >
+        全部
+      </router-link>
+    </li>
+
+    <li
+      v-for="category in categories"
+      :key="category.id"
+      class="nav-item"
+    >
+      <router-link
+        class="nav-link"
+        :to="{ name: 'restaurants', query: { categoryId: category.id } }"
+      >
+        {{ category.name }}
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'RestaurantsNavPills',
+  props: {
+    categories: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -2,6 +2,7 @@
   <div class="container py-5">
     <NavTabs />
     <!-- 餐廳類別標籤 RestaurantsNavPills -->
+    <RestaurantsNavPills :categories="categories" />
 
     <div class="row">
       <!-- 餐廳卡片 RestaurantCard-->
@@ -19,6 +20,7 @@
 <script>
 import NavTabs from './../components/NavTabs'
 import RestaurantCard from './../components/RestaurantCard'
+import RestaurantsNavPills from './../components/RestaurantsNavPills'
 
 const dummyData = {
   restaurants: [
@@ -288,7 +290,8 @@ export default {
   name: 'Restaurants',
   components: {
     NavTabs,
-    RestaurantCard
+    RestaurantCard,
+    RestaurantsNavPills
   },
   data () {
     return {

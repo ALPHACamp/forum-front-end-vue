e27c35376e51d3a874b9f5a01a3521e7163a60a6
pjchender <pjchender@gmail.com>
2019-06-30 21:18:52 +0800

    Install sweetAlert2 and use Toast method

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,7 +1,15 @@
 import axios from 'axios'
+import Swal from 'sweetalert2'
 
 const baseURL = 'http://localhost:3000/api'
 
 export const apiHelper = axios.create({
   baseURL
+})
+
+export const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
 })

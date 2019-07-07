875663f0638121f320220ab9f3ab7adf2a3ff516
pjchender <pjchender@gmail.com>
2019-07-07 12:37:26 +0800

    API integration of RestaurantsTop page

--- src/apis/restaurants.js
+++ src/apis/restaurants.js
@@ -12,5 +12,10 @@ export default {
     return apiHelper.get('/restaurants/feeds', {
       headers: { Authorization: `Bearer ${getToken()}` }
     })
+  },
+  getTopRestaurants () {
+    return apiHelper.get('/restaurants/top', {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
   }
 }

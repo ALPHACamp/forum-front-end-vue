6d9bd748de685f56b6fa9dd84ba48578bc31cece
pjchender <pjchender@gmail.com>
2019-07-12 22:33:31 +0800

    API integration of RestaurantDashboard Page

--- src/views/RestaurantDashboard.vue
+++ src/views/RestaurantDashboard.vue
@@ -20,70 +20,8 @@
 </template>
 
 <script>
-const dummyData = {
-  restaurant: {
-    id: 3,
-    name: 'Abel Blanda',
-    tel: '1-554-475-7456 x210',
-    address: '4510 Guiseppe Lights',
-    opening_hours: '08:00',
-    description:
-      'Quibusdam aliquid perferendis quam. Aspernatur sit dolor aliquid unde iure. Qui voluptatibus aperiam autem fugit corrupti dolore. Nostrum ipsa est quas voluptatibus quia debitis veniam. Libero harum assumenda in impedit. Aliquid consequatur voluptatibus quo tenetur nesciunt enim suscipit voluptatibus.\n \rVitae eos magni nihil. Facilis laborum et labore consequuntur dolores quis aut rem porro. Enim suscipit cumque dolorem odio sint alias. Molestias excepturi esse maxime magnam eius aut non soluta.\n \rEum commodi ea itaque aut quam. Nemo nobis aliquid quis dolorem. Aspernatur aut non consequatur ad perferendis maiores sunt eveniet.',
-    image: 'https://loremflickr.com/320/240/food,dessert,restaurant/?random=3',
-    createdAt: '2019-06-22T09:00:43.000Z',
-    updatedAt: '2019-06-22T09:00:43.000Z',
-    CategoryId: 4,
-    Category: {
-      id: 4,
-      name: '墨西哥料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    },
-    FavoritedUsers: [
-      {
-        id: 1,
-        name: 'root',
-        email: 'root@example.com',
-        password:
-          '$2a$10$3.cGoUF1lkEfQ2Oi57VpC.xVU3/8NS6yJzBQ0VJKV9AeW0e1trmmS',
-        image: 'https://i.imgur.com/JtQJRMZ.png',
-        isAdmin: true,
-        createdAt: '2019-06-22T09:00:43.000Z',
-        updatedAt: '2019-06-23T01:15:48.000Z',
-        Favorite: {
-          UserId: 1,
-          RestaurantId: 3,
-          createdAt: '2019-06-24T15:03:35.000Z',
-          updatedAt: '2019-06-24T15:03:35.000Z'
-        }
-      }
-    ],
-    LikedUsers: [],
-    Comments: [
-      {
-        id: 10,
-        text: 'Nisi quam dolorem debitis deserunt esse praesentium et.',
-        UserId: 1,
-        RestaurantId: 3,
-        createdAt: '2019-06-22T09:00:43.000Z',
-        updatedAt: '2019-06-22T09:00:43.000Z',
-        User: {
-          id: 1,
-          name: 'root',
-          email: 'root@example.com',
-          password:
-            '$2a$10$3.cGoUF1lkEfQ2Oi57VpC.xVU3/8NS6yJzBQ0VJKV9AeW0e1trmmS',
-          image: 'https://i.imgur.com/JtQJRMZ.png',
-          isAdmin: true,
-          createdAt: '2019-06-22T09:00:43.000Z',
-          updatedAt: '2019-06-23T01:15:48.000Z'
-        }
-      }
-    ]
-  },
-  isFavorited: true,
-  isLiked: false
-}
+import restaurantsAPI from './../apis/restaurants'
+import { Toast } from './../utils/helpers'
 
 export default {
   data () {
@@ -98,22 +36,43 @@ export default {
       }
     }
   },
-  mounted () {
+  created () {
     const { id: restaurantId } = this.$route.params
     this.fetchRestaurant(restaurantId)
   },
+  beforeRouteUpdate (to, from, next) {
+    const { id: restaurantId } = to.params
+    this.fetchRestaurant(restaurantId)
+    next()
+  },
   methods: {
-    fetchRestaurant (restaurantId) {
-      const { restaurant } = dummyData
+    async fetchRestaurant (restaurantId) {
+      try {
+        const {
+          data: { restaurant },
+          statusText
+        } = await restaurantsAPI.getRestaurant({
+          restaurantId
+        })
 
-      this.restaurant = {
-        ...this.restaurant,
-        id: restaurant.id,
-        name: restaurant.name,
-        categoryName: restaurant.Category.name,
-        commentsLength: restaurant.Comments.length,
-        favoritedUsersLength: restaurant.FavoritedUsers.length,
-        likedUsersLength: restaurant.LikedUsers.length
+        if (statusText !== 'OK') {
+          throw new Error(statusText)
+        }
+
+        this.restaurant = {
+          ...this.restaurant,
+          id: restaurant.id,
+          name: restaurant.name,
+          categoryName: restaurant.Category.name,
+          commentsLength: restaurant.Comments.length,
+          favoritedUsersLength: restaurant.FavoritedUsers.length,
+          likedUsersLength: restaurant.LikedUsers.length
+        }
+      } catch (error) {
+        Toast.fire({
+          type: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
       }
     }
   }

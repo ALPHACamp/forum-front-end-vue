4ea0e3c1f8131e2046e2418e4bc02ca3a4a1faad
pjchender <pjchender@gmail.com>
2019-08-04 12:27:50 +0800

    Use Spinner in RestaurantsFeeds

--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-
-    <h1 class="mt-5">
-      最新動態
-    </h1>
-    <hr>
-    <div class="row">
-      <div class="col-md-6">
-        <h3>最新餐廳</h3>
-        <NewestRestaurants :restaurants="restaurants" />
+    <Spinner v-if="isLoading" />
+    <template v-else>
+      <h1 class="mt-5">
+        最新動態
+      </h1>
+      <hr>
+      <div class="row">
+        <div class="col-md-6">
+          <h3>最新餐廳</h3>
+          <NewestRestaurants :restaurants="restaurants" />
+        </div>
+        <div class="col-md-6">
+          <h3>最新評論</h3>
+          <NewestComments :comments="comments" />
+        </div>
       </div>
-      <div class="col-md-6">
-        <h3>最新評論</h3>
-        <NewestComments :comments="comments" />
-      </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -23,6 +25,7 @@
 import NavTabs from './../components/NavTabs'
 import NewestRestaurants from './../components/NewestRestaurants'
 import NewestComments from './../components/NewestComments'
+import Spinner from './../components/Spinner'
 import restaurantsAPI from './../apis/restaurants'
 import { Toast } from './../utils/helpers'
 
@@ -30,12 +33,14 @@ export default {
   components: {
     NavTabs,
     NewestRestaurants,
-    NewestComments
+    NewestComments,
+    Spinner
   },
   data () {
     return {
       restaurants: [],
-      comments: []
+      comments: [],
+      isLoading: true
     }
   },
   created () {
@@ -52,7 +57,9 @@ export default {
 
         this.restaurants = data.restaurants
         this.comments = data.comments.filter(comment => comment.Restaurant)
+        this.isLoading = false
       } catch (error) {
+        this.isLoading = false
         Toast.fire({
           type: 'error',
           title: '無法取得最新動態，請稍後再試'

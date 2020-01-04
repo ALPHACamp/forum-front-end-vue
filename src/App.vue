c1c46527af7a44271739d58a0bfa6da2f81aeb05
pjchender <pjchender@gmail.com>
2020-01-04 19:19:38 +0800

    Install bootstrap and related packages

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,15 @@
   </div>
 </template>
 
+<script>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
+export default {
+  name: 'App'
+}
+</script>
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;

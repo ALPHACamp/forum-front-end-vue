a9b00dfbd83bd0444d4ffd6e27f1814fefd1a194
pjchender <pjchender@gmail.com>
2020-01-18 13:24:59 +0800

    Add page - SignIn

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
+import SignIn from '../views/SignIn.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
   {
     path: '*',
     name: 'not-found',

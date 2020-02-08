9e71ea2b6ae7a47944f75c6decde8d544f9cb7aa
pjchender <pjchender@gmail.com>
2020-02-08 20:36:06 +0800

    Add routes and empty page for Restaurants, RestaurantsFeeds, RestaurantsTop, UsersTop

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import NotFound from '../views/NotFound.vue'
 import SignIn from '../views/SignIn.vue'
+import Restaurants from '../views/Restaurants.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'root',
+    redirect: '/restaurants'
+  },
   {
     path: '/signin',
     name: 'sign-in',
@@ -15,6 +21,26 @@ const routes = [
     path: '/signup',
     name: 'sign-up',
     component: () => import('../views/SignUp.vue')
+  },
+  {
+    path: '/restaurants',
+    name: 'restaurants',
+    component: Restaurants
+  },
+  {
+    path: '/restaurants/feeds',
+    name: 'restaurants-feeds',
+    component: () => import('../views/RestaurantsFeeds.vue')
+  },
+  {
+    path: '/restaurants/top',
+    name: 'restaurants-top',
+    component: () => import('../views/RestaurantsTop.vue')
+  },
+  {
+    path: '/users/top',
+    name: 'users-top',
+    component: () => import('../views/UsersTop.vue')
   },
   {
     path: '*',
---
+++ src/views/Restaurants.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container py-5">
+    <h1 class="mt-5">
+      首頁 - 餐廳列表
+    </h1>
+  </div>
+</template>

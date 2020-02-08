fce14fd9fec6906c8ca0760bb8e7adb67cdd7bd9
pjchender <pjchender@gmail.com>
2020-02-08 18:52:36 +0800

    Add component - Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="restaurant-forum">
-    <router-view />
+    <Navbar />
+    <main class="mt-5 bg-white">
+      <router-view />
+    </main>
   </div>
 </template>
 
 <script>
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
+import Navbar from './components/Navbar'
 
 export default {
-  name: 'App'
+  components: {
+    Navbar
+  }
 }
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,97 @@
+<template>
+  <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+    <router-link
+      class="navbar-brand"
+      to="/"
+    >
+      餐廳評論網
+    </router-link>
+
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+
+    <div
+      id="navbarSupportedContent"
+      class="navbar-collapse collapse"
+    >
+      <div class="ml-auto d-flex align-items-center">
+        <!-- is user is admin -->
+        <router-link
+          v-if="currentUser.isAdmin"
+          to="#"
+          class="text-white mr-3"
+        >
+          管理員後台
+        </router-link>
+
+        <!-- is user is login -->
+        <template v-if="isAuthenticated">
+          <router-link
+            to="#"
+            class="text-white mr-3"
+          >
+            {{ currentUser.name || '使用者' }} 您好
+          </router-link>
+          <button
+            type="button"
+            class="btn btn-sm btn-outline-success my-2 my-sm-0"
+          >
+            登出
+          </button>
+        </template>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+/* eslint-disable */
+// 模擬 API 回傳的內容
+const dummyUser = {
+  currentUser: {
+    id: 1,
+    name: '管理者',
+    email: 'root@example.com',
+    image: 'https://i.pravatar.cc/300',
+    isAdmin: true
+  },
+  isAuthenticated: true
+}
+
+export default {
+  data() {
+    return {
+      currentUser: {
+        id: -1,
+        name: '',
+        email: '',
+        image: '',
+        isAdmin: false
+      },
+      isAuthenticated: false
+    }
+  },
+  methods: {
+    // 會向後端 API 拉取資料
+    fetchUser() {
+      this.currentUser = {
+        ...this.currentUser,
+        ...dummyUser.currentUser
+      }
+      this.isAuthenticated = dummyUser.isAuthenticated
+    }
+  },
+  created() {
+    this.fetchUser()
+  }
+}
+</script>

067bd0a96bb1ec12d031384631aa6ab09a7deeb4
pjchender <pjchender@gmail.com>
2020-02-16 12:49:57 +0800

    Add component - NewestRestaurants

---
+++ src/components/NewestRestaurants.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      最新餐廳
+    </div>
+    <div class="card-body">
+      <div>
+        <h4>
+          <a href="#">Deangelo Farrell</a>
+          <small>中式料理</small>
+        </h4>
+        <p>
+          Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui
+          deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo
+          necessitatibus. Veniam sint explicabo. Est velit soluta.
+        </p>
+        11 days ago
+        <hr>
+      </div>
+      <div>
+        <h4>
+          <a href="#">Carter Howe</a>
+          <small>日本料理</small>
+        </h4>
+        <p>
+          Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui
+          deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo
+          necessitatibus. Veniam sint explicabo. Est velit soluta.
+        </p>
+        11 days ago
+        <hr>
+      </div>
+      <div>
+        <h4>
+          <a href="#">Dr. Carole Smith</a>
+          <small>義大利料理</small>
+        </h4>
+        <p>
+          Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui
+          deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo
+          necessitatibus. Veniam sint explicabo. Est velit soluta.
+        </p>
+        11 days ago
+        <hr>
+      </div>
+      <div>
+        <h4>
+          <a href="#">Nyah Bernier</a>
+          <small>素食料理</small>
+        </h4>
+        <p>
+          Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui
+          deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo
+          necessitatibus. Veniam sint explicabo. Est velit soluta.
+        </p>
+        11 days ago
+        <hr>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="container py-5">
     <NavTabs />
+
     <h1 class="mt-5">
       最新動態
     </h1>
+    <hr>
+    <div class="row">
+      <div class="col-md-6">
+        <h3>最新餐廳</h3>
+        <!-- 最新餐廳 NewestRestaurants -->
+        <NewestRestaurants />
+      </div>
+      <div class="col-md-6">
+        <!-- 最新評論 NewestComments-->
+        <h3>最新評論</h3>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import NavTabs from './../components/NavTabs'
+import NewestRestaurants from './../components/NewestRestaurants'
 
 export default {
   components: {
-    NavTabs
+    NavTabs,
+    NewestRestaurants
   }
 }
 </script>

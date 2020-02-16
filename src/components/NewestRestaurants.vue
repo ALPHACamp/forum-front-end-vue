f31f7429b09dcb54d6b3f894129ea285f3020525
pjchender <pjchender@gmail.com>
2020-02-16 14:25:48 +0800

    Pass data from RestaruantsFeeds into NewestRestaurants by props

--- src/components/NewestRestaurants.vue
+++ src/components/NewestRestaurants.vue
@@ -4,58 +4,39 @@
       最新餐廳
     </div>
     <div class="card-body">
-      <div>
+      <div
+        v-for="restaurant in restaurants"
+        :key="restaurant.id"
+      >
         <h4>
-          <a href="#">Deangelo Farrell</a>
-          <small>中式料理</small>
+          <a href="#">{{ restaurant.name }}</a>
+          <small>{{
+            restaurant.Category ? restaurant.Category.name : '未分類'
+          }}</small>
         </h4>
         <p>
-          Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui
-          deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo
-          necessitatibus. Veniam sint explicabo. Est velit soluta.
+          {{ restaurant.description }}
         </p>
-        11 days ago
-        <hr>
-      </div>
-      <div>
-        <h4>
-          <a href="#">Carter Howe</a>
-          <small>日本料理</small>
-        </h4>
-        <p>
-          Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui
-          deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo
-          necessitatibus. Veniam sint explicabo. Est velit soluta.
-        </p>
-        11 days ago
-        <hr>
-      </div>
-      <div>
-        <h4>
-          <a href="#">Dr. Carole Smith</a>
-          <small>義大利料理</small>
-        </h4>
-        <p>
-          Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui
-          deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo
-          necessitatibus. Veniam sint explicabo. Est velit soluta.
-        </p>
-        11 days ago
-        <hr>
-      </div>
-      <div>
-        <h4>
-          <a href="#">Nyah Bernier</a>
-          <small>素食料理</small>
-        </h4>
-        <p>
-          Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui
-          deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo
-          necessitatibus. Veniam sint explicabo. Est velit soluta.
-        </p>
-        11 days ago
+        {{ restaurant.createdAt }}
         <hr>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    restaurants: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+small {
+  font-size: 10px;
+}
+</style>
--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -10,7 +10,7 @@
       <div class="col-md-6">
         <h3>最新餐廳</h3>
         <!-- 最新餐廳 NewestRestaurants -->
-        <NewestRestaurants />
+        <NewestRestaurants :restaurants="restaurants" />
       </div>
       <div class="col-md-6">
         <!-- 最新評論 NewestComments-->

ed0f99d14489a36c58cea69223ccc3ee4c6c6f86
pjchender <pjchender@gmail.com>
2020-02-16 14:58:59 +0800

    Use Vue Filters with momemtjs to format time

--- src/components/NewestRestaurants.vue
+++ src/components/NewestRestaurants.vue
@@ -10,14 +10,14 @@
       >
         <h4>
           <a href="#">{{ restaurant.name }}</a>
-          <small>{{
+          <small>&nbsp;{{
             restaurant.Category ? restaurant.Category.name : '未分類'
           }}</small>
         </h4>
         <p>
           {{ restaurant.description }}
         </p>
-        {{ restaurant.createdAt }}
+        {{ restaurant.createdAt | fromNow }}
         <hr>
       </div>
     </div>
@@ -25,7 +25,19 @@
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
+  filters: {
+    fromNow (datetime) {
+      if (!datetime) {
+        return '-'
+      }
+
+      // 使用 moment 回傳 fromNow 的時間
+      return moment(datetime).fromNow()
+    }
+  },
   props: {
     restaurants: {
       type: Array,

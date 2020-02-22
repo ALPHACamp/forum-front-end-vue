b024f437a448cd0b29d80ecea649c8a112976165
pjchender <pjchender@gmail.com>
2020-02-22 16:26:17 +0800

    Add component - RestaurantComments

---
+++ src/components/RestaurantComments.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h2 class="my-4">
+      所有評論：
+    </h2>
+
+    <div
+      v-for="comment in restaurantComments"
+      :key="comment.id"
+    >
+      <blockquote class="blockquote mb-0">
+        <button
+          v-if="currentUser.isAdmin"
+          type="button"
+          class="btn btn-danger float-right"
+        >
+          Delete
+        </button>
+        <h3>
+          <a href="#">
+            {{ comment.User.name }}
+          </a>
+        </h3>
+        <p>{{ comment.text }}</p>
+        <footer class="blockquote-footer">
+          {{ comment.createdAt | fromNow }}
+        </footer>
+      </blockquote>
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+
+const dummyUser = {
+  currentUser: {
+    id: 1,
+    name: 'root1',
+    email: 'root@example.com',
+    isAdmin: true
+  },
+  isAuthenticated: true
+}
+
+export default {
+  name: 'RestaurantComments',
+  filters: {
+    fromNow (datetime) {
+      if (!datetime) {
+        return '-'
+      }
+
+      return moment(datetime).fromNow()
+    }
+  },
+  props: {
+    restaurantComments: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      currentUser: dummyUser.currentUser
+    }
+  }
+}
+</script>
--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -7,12 +7,14 @@
     />
     <hr>
     <!-- 餐廳評論 RestaurantComments -->
+    <RestaurantComments :restaurant-comments="restaurantComments" />
     <!-- 新增評論 CreateComment -->
   </div>
 </template>
 
 <script>
 import RestaurantDetail from './../components/RestaurantDetail'
+import RestaurantComments from './../components/RestaurantComments'
 
 const dummyData = {
   restaurant: {
@@ -134,7 +136,8 @@ const dummyData = {
 export default {
   name: 'Restaurant',
   components: {
-    RestaurantDetail
+    RestaurantDetail,
+    RestaurantComments
   },
   data () {
     return {

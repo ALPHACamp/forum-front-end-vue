6ca3f4ce4b09c089a42c3481e137e598dc8e4a95
pjchender <pjchender@gmail.com>
2020-02-23 19:56:06 +0800

    Add page - AdminRestaurants

--- src/router/index.js
+++ src/router/index.js
@@ -58,6 +58,16 @@ const routes = [
     component: () => import('../views/User.vue')
   },
   {
+    path: '/admin',
+    exact: true,
+    redirect: '/admin/restaurants'
+  },
+  {
+    path: '/admin/restaurants',
+    name: 'admin-restaurants',
+    component: () => import('../views/AdminRestaurants.vue')
+  },
+  {
     path: '*',
     name: 'not-found',
     component: NotFound
---
+++ src/views/AdminRestaurants.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container py-5">
+    <!-- 後台導覽頁籤 AdminNav -->
+
+    <a
+      href="#"
+      class="btn btn-primary mb-4"
+    >
+      New Restaurant
+    </a>
+
+    <!-- 後台餐廳列表 AdminRestaurantsTable -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'AdminRestaurants'
+}
+</script>

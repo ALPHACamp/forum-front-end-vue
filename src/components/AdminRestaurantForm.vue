057a9bdcc6b213b0563f5fcb30f38ceb94d1df66
pjchender <pjchender@gmail.com>
2020-02-23 21:25:08 +0800

    Add component - AdminRestaurantForm

---
+++ src/components/AdminRestaurantForm.vue
@@ -0,0 +1,102 @@
+<template>
+  <form>
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input
+        id="name"
+        type="text"
+        class="form-control"
+        name="name"
+        placeholder="Enter name"
+        required
+      >
+    </div>
+
+    <div class="form-group">
+      <label for="categoryId">Category</label>
+      <select
+        id="categoryId"
+        class="form-control"
+        name="categoryId"
+        required
+      >
+        <option
+          value=""
+          selected
+          disabled
+        >
+          --請選擇--
+        </option>
+        <option value="1">
+          中式料理
+        </option>
+        <option value="2">
+          日式料理
+        </option>
+        <option value="3">
+          義式料理
+        </option>
+      </select>
+    </div>
+
+    <div class="form-group">
+      <label for="tel">Tel</label>
+      <input
+        id="tel"
+        type="text"
+        class="form-control"
+        name="tel"
+        placeholder="Enter telephone number"
+      >
+    </div>
+
+    <div class="form-group">
+      <label for="address">Address</label>
+      <input
+        id="address"
+        type="text"
+        class="form-control"
+        placeholder="Enter address"
+        name="address"
+      >
+    </div>
+
+    <div class="form-group">
+      <label for="opening-hours">Opening Hours</label>
+      <input
+        id="opening-hours"
+        type="time"
+        class="form-control"
+        name="opening_hours"
+      >
+    </div>
+
+    <div class="form-group">
+      <label for="description">Description</label>
+      <textarea
+        id="description"
+        class="form-control"
+        rows="3"
+        name="description"
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="image">Image</label>
+      <input
+        id="image"
+        type="file"
+        name="image"
+        accept="image/*"
+        class="form-control-file"
+      >
+    </div>
+
+    <button
+      type="submit"
+      class="btn btn-primary"
+    >
+      送出
+    </button>
+  </form>
+</template>

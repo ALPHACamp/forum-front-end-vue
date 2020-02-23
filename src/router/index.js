2a7a0d83cf9403a194e454c1db4e73cbaeed7497
pjchender <pjchender@gmail.com>
2020-02-23 20:24:41 +0800

    Add page - AdminRestaurant

--- src/router/index.js
+++ src/router/index.js
@@ -68,6 +68,11 @@ const routes = [
     component: () => import('../views/AdminRestaurants.vue')
   },
   {
+    path: '/admin/restaurants/:id',
+    name: 'admin-restaurant',
+    component: () => import('../views/AdminRestaurant.vue')
+  },
+  {
     path: '*',
     name: 'not-found',
     component: NotFound
---
+++ src/views/AdminRestaurant.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="container py-5">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>{{ restaurant.name }}</h1>
+        <span
+          class="badge badge-secondary mt-1 mb-3"
+        >{{ restaurant.categoryName }}</span>
+      </div>
+      <div class="col-md-4">
+        <img
+          class="img-responsive center-block"
+          :src="restaurant.image"
+          style="width: 250px;margin-bottom: 25px;"
+        >
+        <div class="well">
+          <ul class="list-unstyled">
+            <li>
+              <strong>Opening Hour:</strong>
+              {{ restaurant.openingHours }}
+            </li>
+            <li>
+              <strong>Tel:</strong>
+              {{ restaurant.tel }}
+            </li>
+            <li>
+              <strong>Address:</strong>
+              {{ restaurant.address }}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-8">
+        <p>{{ restaurant.description }}</p>
+      </div>
+    </div>
+    <hr>
+
+    <button
+      type="button"
+      class="btn btn-link"
+      @click="$router.back()"
+    >
+      回上一頁
+    </button>
+  </div>
+</template>
+
+<script>
+const dummyData = {
+  restaurant: {
+    id: 2,
+    name: 'Kaci Koelpin',
+    tel: '083-343-7765',
+    address: '22769 Johnson Squares',
+    opening_hours: '08:00',
+    description:
+      'Sequi facere sit iusto molestiae dolores quibusdam dolores. Labore deleniti beatae id. Qui quidem quibusdam fugiat qui harum odit voluptas. Beatae asperiores exercitationem quia commodi. Provident magnam sed expedita quibusdam labore aliquid hic.',
+    image:
+      'https://loremflickr.com/320/240/restaurant,food/?random=22.957592747300982',
+    viewCounts: 123,
+    createdAt: '2019-11-20T06:25:42.922Z',
+    updatedAt: '2020-01-19T02:39:43.017Z',
+    CategoryId: null,
+    Category: null
+  }
+}
+
+export default {
+  name: 'AdminRestaurant',
+  data () {
+    return {
+      restaurant: {
+        id: -1,
+        name: '',
+        categoryName: '',
+        image: '',
+        openingHours: '',
+        tel: '',
+        address: '',
+        description: ''
+      }
+    }
+  },
+  mounted () {
+    const { id: restaurantId } = this.$route.params
+    this.fetchRestaurant(restaurantId)
+  },
+  methods: {
+    fetchRestaurant (restaurantId) {
+      const { restaurant } = dummyData
+      const {
+        id,
+        name,
+        Category,
+        image,
+        opening_hours: openingHours,
+        tel,
+        address,
+        description
+      } = restaurant
+
+      this.restaurant = {
+        ...this.restaurant,
+        id,
+        name,
+        categoryName: Category ? Category.name : '未分類',
+        image,
+        openingHours,
+        tel,
+        address,
+        description
+      }
+    }
+  }
+}
+</script>

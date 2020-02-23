201c41c882bd6b6bc36b0b3587bb3fed4f8cd44f
pjchender <pjchender@gmail.com>
2020-02-23 18:23:13 +0800

    fix: update links of user

--- src/components/NewestComments.vue
+++ src/components/NewestComments.vue
@@ -17,9 +17,9 @@
         </h4>
         <p>{{ comment.text }}</p>
         by
-        <a href="#">
+        <router-link :to="{ name: 'user', params: { id: comment.User.id } }">
           {{ comment.User.name }}
-        </a>
+        </router-link>
         at {{ comment.createdAt | fromNow }}
         <hr>
       </div>

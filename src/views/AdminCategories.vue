71cb04d1d6b436aa4a09f9228884eed5ffb72e39
pjchender <pjchender@gmail.com>
2020-02-28 13:44:55 +0800

    Add feature - AdminCategories with edit and cancel features

--- src/views/AdminCategories.vue
+++ src/views/AdminCategories.vue
@@ -52,16 +52,44 @@
             {{ category.id }}
           </th>
           <td class="position-relative">
-            <div class="category-name">
+            <div
+              v-show="!category.isEditing"
+              class="category-name"
+            >
               {{ category.name }}
             </div>
+            <input
+              v-show="category.isEditing"
+              v-model="category.name"
+              type="text"
+              class="form-control"
+            >
+            <span
+              v-show="category.isEditing"
+              class="cancel"
+              @click.stop.prevent="handleCancel(category.id)"
+            >
+              ✕
+            </span>
           </td>
           <td class="d-flex justify-content-between">
             <button
+              v-show="!category.isEditing"
               type="button"
               class="btn btn-link mr-2"
+              @click.stop.prevent="toggleIsEditing(category.id)"
             >
               Edit
+            </button>
+            <button
+              v-show="category.isEditing"
+              type="button"
+              class="btn btn-link mr-2"
+              @click.stop.prevent="
+                updateCategory({ categoryId: category.id, name: category.name })
+              "
+            >
+              Save
             </button>
             <button
               type="button"
@@ -126,7 +154,11 @@ export default {
   },
   methods: {
     fetchCategories () {
-      this.categories = dummyData.categories
+      this.categories = dummyData.categories.map(category => ({
+        ...category,
+        isEditing: false,
+        nameCached: ''
+      }))
     },
     createCategory () {
       // TODO: 透過 API 向後端伺服器新增餐廳類別
@@ -141,8 +173,74 @@ export default {
     deleteCategory (categoryId) {
       // TODO: 透過 API 向後端伺服器刪除餐廳類別
 
-      this.categories = this.categories.filter(category => category.id !== categoryId)
+      this.categories = this.categories.filter(
+        category => category.id !== categoryId
+      )
+    },
+    updateCategory ({ categoryId, name }) {
+      // TODO: 透過 API 向伺服器更新類別名稱
+
+      this.toggleIsEditing(categoryId)
+    },
+    toggleIsEditing (categoryId) {
+      this.categories = this.categories.map(category => {
+        if (category.id === categoryId) {
+          return {
+            ...category,
+            isEditing: !category.isEditing,
+            nameCached: category.name
+          }
+        }
+
+        return category
+      })
+    },
+    handleCancel (categoryId) {
+      this.categories = this.categories.map(category => {
+        if (category.id === categoryId) {
+          return {
+            ...category,
+
+            // 把原本的餐廳類別名稱還回去
+            name: category.nameCached
+          }
+        }
+
+        return category
+      })
+
+      this.toggleIsEditing(categoryId)
     }
   }
 }
 </script>
+
+<style scoped>
+.category-name {
+  padding: 0.375rem 0.75rem;
+  border: 1px solid transparent;
+  outline: 0;
+  cursor: auto;
+}
+
+.btn-link {
+  width: 62px;
+}
+
+.cancel {
+  position: absolute;
+  right: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 25px;
+  height: 25px;
+  border: 1px solid #aaaaaa;
+  border-radius: 50%;
+  user-select: none;
+  cursor: pointer;
+  font-size: 12px;
+}
+</style>

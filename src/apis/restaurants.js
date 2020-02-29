071f075796ed1cbfb563fb5093ce4e57095d5098
pjchender <pjchender@gmail.com>
2020-02-29 14:32:03 +0800

    Add API - getFeeds in restaurants

--- src/apis/restaurants.js
+++ src/apis/restaurants.js
@@ -8,5 +8,10 @@ export default {
     return apiHelper.get(`/restaurants?${searchParams.toString()}`, {
       headers: { Authorization: `Bearer ${getToken()}` }
     })
+  },
+  getFeeds () {
+    return apiHelper.get('/restaurants/feeds', {
+      headers: { Authorization: `Bearer ${getToken()}` }
+    })
   }
 }

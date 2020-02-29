cada5c651679194793a1d7be636162a7d6b862b3
pjchender <pjchender@gmail.com>
2020-02-29 15:27:35 +0800

    Add API - addLike and deleteLike in users

--- src/apis/users.js
+++ src/apis/users.js
@@ -15,5 +15,19 @@ export default {
         Authorization: `Bearer ${getToken()}`
       }
     })
+  },
+  addLike ({ restaurantId }) {
+    return apiHelper.post(`/like/${restaurantId}`, null, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      }
+    })
+  },
+  deleteLike ({ restaurantId }) {
+    return apiHelper.delete(`/like/${restaurantId}`, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      }
+    })
   }
 }

f9a32c82eacfb977362ff0bfdd19f8711e6ed1b4
pjchender <pjchender@gmail.com>
2020-03-01 18:47:08 +0800

    Add API - getCurrentUser and get in users

--- src/apis/users.js
+++ src/apis/users.js
@@ -2,6 +2,20 @@ import { apiHelper } from './../utils/helpers'
 const getToken = () => localStorage.getItem('token')
 
 export default {
+  getCurrentUser () {
+    return apiHelper.get('/get_current_user', {
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      }
+    })
+  },
+  get ({ userId }) {
+    return apiHelper.get(`/users/${userId}`, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`
+      }
+    })
+  },
   addFavorite ({ restaurantId }) {
     return apiHelper.post(`/favorite/${restaurantId}`, null, {
       headers: {

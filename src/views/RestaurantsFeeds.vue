a554a93d47781798f445b2b57a42d525d73f1fbe
pjchender <pjchender@gmail.com>
2020-03-08 21:07:59 +0800

    fix: use Spinner in RestaurantsFeeds

--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="container py-5">
     <NavTabs />
-
-    <h1 class="mt-5">
-      最新動態
-    </h1>
-    <hr>
-    <div class="row">
-      <div class="col-md-6">
-        <h3>最新餐廳</h3>
-        <!-- 最新餐廳 NewestRestaurants -->
-        <NewestRestaurants :restaurants="restaurants" />
+    <Spinner v-if="isLoading" />
+    <template v-else>
+      <h1 class="mt-5">
+        最新動態
+      </h1>
+      <hr>
+      <div class="row">
+        <div class="col-md-6">
+          <h3>最新餐廳</h3>
+          <!-- 最新餐廳 NewestRestaurants -->
+          <NewestRestaurants :restaurants="restaurants" />
+        </div>
+        <div class="col-md-6">
+          <!-- 最新評論 NewestComments-->
+          <h3>最新評論</h3>
+          <NewestComments :comments="comments" />
+        </div>
       </div>
-      <div class="col-md-6">
-        <!-- 最新評論 NewestComments-->
-        <h3>最新評論</h3>
-        <NewestComments :comments="comments" />
-      </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -25,6 +27,7 @@
 import NavTabs from './../components/NavTabs'
 import NewestRestaurants from './../components/NewestRestaurants'
 import NewestComments from './../components/NewestComments'
+import Spinner from './../components/Spinner'
 import restaurantsAPI from './../apis/restaurants'
 import { Toast } from './../utils/helpers'
 
@@ -33,12 +36,14 @@ export default {
   components: {
     NavTabs,
     NewestRestaurants,
-    NewestComments
+    NewestComments,
+    Spinner
   },
   data () {
     return {
       restaurants: [],
-      comments: []
+      comments: [],
+      isLoading: true
     }
   },
   created () {
@@ -47,12 +52,17 @@ export default {
   methods: {
     async fetchFeeds () {
       try {
+        this.isLoading = true
         const { data } = await restaurantsAPI.getFeeds()
         const { restaurants, comments } = data
 
         this.restaurants = restaurants
-        this.comments = comments.filter(comment => comment.Restaurant && comment.text)
+        this.comments = comments.filter(
+          comment => comment.Restaurant && comment.text
+        )
+        this.isLoading = false
       } catch (error) {
+        this.isLoading = false
         Toast.fire({
           icon: 'error',
           title: '無法取得最新動態，請稍後再試'
